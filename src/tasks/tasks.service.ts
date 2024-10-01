952d1d2db3c16e471b551c581e9fa50d0e7b8545
tooyangski <apungan.aubreyivansunga@gmail.com>
2024-10-01 18:44:13 +0800

    feat: add class validator for dtos

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
@@ -47,7 +47,11 @@ export class TasksService {
   }
 
   getTaskById(id: string) {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) throw new NotFoundException(`Task with id: ${id} is not found`);
+
+    return task;
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
@@ -57,6 +61,10 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) throw new NotFoundException(`Task with id: ${id} is not found`);
+
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
